b51caa4eefd08983bd3ce6b90f18f70ff478846e
SUBHRANIL SAMANTA <48993245+subhranil66@users.noreply.github.com>
2019-11-01 17:48:09 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,448 @@
+var canvas = document.getElementById('game');
+var context = canvas.getContext('2d');
+var grid = 16;
+var count = 0;
+// var lastMove = 'right';
+// var score = 0;
+var coArray = [];
+var isPaused = true;
+//var scorePrint = document.getElementById('scorePrint');
+
+
+function createCoOrdinateArray()
+{
+    if(!localStorage.coordinate || JSON.parse(localStorage.coordinate) == [])
+    {
+        localStorage.coordinate = JSON.stringify([{"x_coord":160,"y_coord":160,"x_app":320,"y_app":320,"scores":0,"lastMoves":'right',"cell":[{x: 160, y: 160}, {x: 144, y: 160}]}]);
+    }
+    else
+    {
+        coArray = JSON.parse(localStorage.coordinate);
+    }
+}
+createCoOrdinateArray();
+
+
+var lsArray = localStorage.getItem('coordinate');
+var lsarr = JSON.parse(lsArray);
+
+// var snake = 
+// {
+//     x : 160,
+//     y: 160,
+//     cells: [{x:160, y:160},{x:144, y:160}],
+//     die: false,
+//     eatApple: false,
+// };
+
+// var apple = 
+// {
+//     x: 320,
+//     y: 320
+// };
+
+var lastMove = lsarr[lsarr.length-1].lastMoves;
+
+//var score = 0;
+var score = lsarr[lsarr.length-1].scores;
+document.getElementById("scorePrint").innerHTML = "Score = " +  score;
+
+var snake = 
+{
+    x : lsarr[lsarr.length-1].x_coord,
+    y : lsarr[lsarr.length-1].y_coord,
+    cells : lsarr[lsarr.length-1].cell,
+    die: false,
+    eatApple: false
+};
+
+var apple = 
+{
+    x : lsarr[lsarr.length-1].x_app,
+    y : lsarr[lsarr.length-1].y_app
+};
+
+
+
+//draw apple
+function drawApple()
+{
+    context.fillStyle = 'red';
+    context.fillRect(apple.x, apple.y, grid-1, grid-1);
+}
+drawApple();
+
+//draw snake
+function drawSnake()
+{
+    context.fillStyle = 'green';
+    for(let i=0;i<snake.cells.length;i++)
+    context.fillRect(snake.cells[i].x, snake.cells[i].y, grid-1, grid-1);
+}
+drawSnake();
+
+function moveSnake(dx, dy)
+{
+    snake.x += dx;
+    snake.y += dy;
+    
+    snake.cells.unshift({x: snake.x, y: snake.y});
+    if(!snake.eatApple)snake.cells.pop();
+    else snake.eatApple = false;
+}
+
+function checkBoundary()
+{
+    if(snake.x < 0 || snake.x >= canvas.width || snake.y < 0 || snake.y >= canvas.height)
+    {
+        snake.die = true;
+    }
+}
+
+function getRandomInt()
+{
+    return Math.floor(Math.random() * 25) * grid;
+}
+
+function checkAppleIsOnSnake()
+{
+    for(let  i=0;i<snake.cells.length;i++)
+    {
+        if(apple.x == snake.cells[i].x && apple.y == snake.cells[i].y)
+        {
+        return true;
+        }
+    }
+}
+
+function checkSnakeBiteItself()
+{
+    for(let i=4;i<snake.cells.length;i++)
+    {
+        if(snake.x == snake.cells[i].x && snake.y == snake.cells[i].y)
+        {
+            snake.die = true;
+        }
+    } 
+}
+
+function generateApple()
+{
+    apple.x = getRandomInt();
+    apple.y = getRandomInt();
+    if(checkAppleIsOnSnake())
+    {
+        generateApple();
+    }
+}
+
+function checkEatApple()
+{
+    if(snake.x == apple.x && snake.y == apple.y)
+    {
+        snake.eatApple = true;
+        generateApple();
+        drawApple();
+        countScore();
+    }
+}
+
+function countScore()
+{
+    score = score + 10;
+    document.getElementById("scorePrint").innerHTML = "Score = " +  score;
+}
+
+function gameOver()
+{
+    context.clearRect(0, 0, canvas.width,canvas.height);
+    snake.cells = [];
+    context.fillStyle = 'red';
+    context.font = "30px Arial";
+    context.textAlign = "center";
+    context.fillText("Game Over", canvas.width/2, canvas.height/2);
+    localStorage.removeItem("coordinate");
+    coArray = [];
+}
+
+// document.addEventListener('keydown', function(e){
+//     let dx = 0, dy = 0;
+//     if(e.keyCode == 37 && lastMove != 'right')
+//     {
+//         dx = -grid;
+//         dy = 0;
+//         lastMove = 'left';
+//     }
+//     else if(e.keyCode == 38 && lastMove != 'down')
+//     {
+//         dy = -grid;
+//         dx = 0;
+//         lastMove = 'up';
+//     }
+//     else if(e.keyCode == 39 && lastMove != 'left')
+//     {
+//         dx = grid;
+//         dy = 0;
+//         lastMove = 'right';
+//     }
+//     else if(e.keyCode == 40 && lastMove != 'up')
+//     {
+//         dy = grid;
+//         dx = 0;
+//         lastMove = 'down';
+//     }
+//     else
+//     {
+//         return;
+//     }
+//     context.clearRect(0, 0, canvas.width, canvas.height);
+//     moveSnake(dx, dy);
+//     checkBoundary();
+//     checkSnakeBiteItself();
+//     checkEatApple();
+//     if(snake.die)
+//     {
+//         gameOver();
+//         return;
+//     }
+//     drawSnake();
+//     drawApple();
+// });
+
+
+
+var btnToPause = document.getElementById('btnToPause');
+var btnToRenew = document.getElementById('btnToRenew');
+var count=0;
+var paused = false;
+
+
+function move(i)
+{
+    let dx = 0, dy = 0;
+    while(i==1)
+    {
+        if(lastMove != 'right'){
+        dx = -grid;
+        dy = 0;
+        lastMove = 'left';
+        }
+         else if(lastMove == 'right'){
+        //     dx += grid;
+        // dy = 0;
+        pauseGame();
+         }
+        break;
+    }
+    while(i==2)
+    {
+        if(lastMove != 'down'){
+        dy = -grid;
+        dx = 0;
+        lastMove = 'up';
+        }
+         else if(lastMove == 'down'){
+        //     dy += grid;
+        // dx = 0;
+        pauseGame();
+         }
+        break;
+    }
+    while(i==3)
+    {
+        if(lastMove != 'left'){
+        dx = grid;
+        dy = 0;
+        lastMove = 'right';
+        }
+         else if(lastMove == 'left'){
+        //     dx -= grid;
+        // dy = 0;
+        pauseGame();
+         }
+        break;
+    }
+    while(i==4)
+    {
+        if(lastMove != 'up'){
+        dy = grid;
+        dx = 0;
+        lastMove = 'down';
+        }
+         else if(lastMove == 'up'){
+        //     dy -= grid;
+        // dx = 0;
+        pauseGame();
+         }
+        break;
+    }
+
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    moveSnake(dx, dy);
+    checkBoundary();
+    checkSnakeBiteItself();
+    checkEatApple();
+    if(snake.die)
+    {
+        gameOver();
+        return;
+    }
+    drawSnake();
+    drawApple();
+}
+
+
+
+function main()
+{
+    var i=0;
+    var interval;
+document.addEventListener('keydown', function(event){
+    if(interval) clearInterval(interval);
+
+    var e = event.which;
+    
+    if(e == 37)
+    {
+        i=1;
+    }
+    else if(e == 38)
+    {
+        i=2;
+    }
+    else if(e == 39)
+    {
+        i=3;
+    }
+    else if(e == 40)
+    {
+        i=4
+    }
+ /*   else
+    {
+        i=0;
+    }*/
+
+    interval = setInterval(function(){
+        move(i);
+    },200);
+});
+
+
+btnToPause.addEventListener('click', function(){
+    count++;
+    if(snake.x>=0 && snake.x<=400 && snake.y>=0 && snake.y<=400)
+    {
+        
+        if(count%2==1)
+        {
+            localStorage.removeItem("coordinate");
+            coArray = [];
+            document.getElementById("btnToPause").innerHTML = "Play";
+            document.getElementById("btnToPause").style.backgroundColor = "#008CBA";
+            pauseGame();
+        
+            addCoOrdinateToLS();
+        }
+        else if(count%2==0 && count>0)
+        {
+            document.getElementById("btnToPause").innerHTML = "Pause";
+            document.getElementById("btnToPause").style.backgroundColor = "#f44336";
+            // localStorage.removeItem("coordinate");
+            // coArray = [];
+           interval = setInterval(function(){
+            move(i);
+        },200);
+        }
+    }
+});
+
+btnToRenew.addEventListener('click', function(){
+    window.location.href = ("index.html");
+    localStorage.removeItem("coordinate");
+});
+
+/*
+document.addEventListener('keydown', function(e){
+    if(e.keyCode == 32) 
+    {
+        count++;
+        
+        if(snake.x>=0 && snake.x<=400 && snake.y>=0 && snake.y<=400)
+    {
+        if(count%2==1)
+        {
+            pauseGame(interval);
+
+        addCoOrdinateToLS();
+        }
+        else if(count%2==0 && count>0)
+        {
+            interval = setInterval(function(){
+                move(i);
+            },200);
+        }
+    }
+    }
+});
+*/
+
+document.addEventListener('keydown', function(e){
+    if(snake.x>=0 && snake.x<=400 && snake.y>=0 && snake.y<=400)
+        {
+    if(e.keyCode == 32)
+    {
+        localStorage.removeItem("coordinate");
+            coArray = [];
+        pauseGame();
+        addCoOrdinateToLS();
+    }
+    else if(e.keyCode == 13)
+    {
+        // localStorage.removeItem("coordinate");
+        //     coArray = [];
+        move(i);
+    }
+}
+});
+
+
+function pauseGame()
+{
+   // clearTimeout(interval);
+   clearInterval(interval);
+}
+
+}
+main();
+
+
+
+
+
+
+// function addCoOrdinateToLS()
+// {
+//     var coObject = new Object();
+
+//     coObject.x_coord = snake.x;
+//     coObject.y_coord = snake.y;
+
+//     coArray.push(coObject);
+//     localStorage.coordinate = JSON.stringify(coArray);
+// }
+
+function addCoOrdinateToLS()
+{
+    var coObject = {
+        x_coord : snake.x,
+        y_coord : snake.y,
+        x_app : apple.x,
+        y_app : apple.y,
+        scores : score,
+        lastMoves : lastMove,
+        cell : snake.cells
+    }
+    coArray.push(coObject);
+    localStorage.coordinate = JSON.stringify(coArray);
+}
